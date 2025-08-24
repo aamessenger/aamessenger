bd5563cbdbe1998908f19d1b43c36a23452aa49b
andrejsgrisins <andrejsgrisins2004@gmail.com>
2025-08-24 18:24:38 +0300

    fix

--- database/init-scripts/01-init-dn.sh
+++ database/init-scripts/01-init-dn.sh
@@ -1,8 +1,23 @@
 # Copyright (c) 2023 Andrejs Grišins, Anastasia Petrova. Unauthorized use prohibited.
 
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    last_online TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-);
+#!/bin/bash
+set -e  # Exit on any error
+
+# Start PostgreSQL in the background to perform initialization
+gosu postgres pg_ctl -D "$PGDATA" -w start
+
+# Create a user and database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER myuser WITH PASSWORD 'mypassword';
+    CREATE DATABASE mydb OWNER myuser;
+    GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
+EOSQL
+
+# Execute custom SQL files (if any)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "mydb" -f 02-sample-data.sql
+
+# Stop PostgreSQL after initialization
+gosu postgres pg_ctl -D "$PGDATA" stop
+
+# Allow container to start normally via the standard entrypoint
+exec "$@"
