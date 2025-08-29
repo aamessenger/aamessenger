731497c3cfd6f4d9a4725c545cccbd6c799c4533
andrejsgrisins <andrejsgrisins2004@gmail.com>
2025-08-29 12:55:51 +0300

    Guest users added; database changed.

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -1,12 +1,14 @@
-# Copyright (c) 2023 Andrejs Grišins, Anastasia Petrova. Unauthorized use prohibited.
+# Copyright (c) 2025 Andrejs Grišins, Anastasia Petrova. Unauthorized use prohibited.
 
 import bcrypt
 import psycopg2
+import random
+import string
 
 connection = psycopg2.connect(dbname="postgres", user="postgres", password="databasepassword", host="localhost", port=5432)
 cursor = connection.cursor()
 
-def create_user_with_email(username: str, email: str, password:str) -> str:
+def create_user_with_email(username: str, email: str, password:str):
     check_unique = cursor.execute((
         """SELECT * FROM users WHERE username = %s OR email = %s"""
     ), (username, email))
@@ -16,10 +18,25 @@ def create_user_with_email(username: str, email: str, password:str) -> str:
     password_hash = hash_password(password)
 
     cursor.execute((
-        """INSERT INTO users (username, email, password_hash) VALUES (%s, %s, %s)"""
+        """INSERT INTO users (guest, username, email, password_hash) VALUES (FALSE, %s, %s, %s)"""
     ), (username, email, password_hash))
-    return("success")
     pass
+
+def create_user_guest():
+    check_unique = False
+    while not check_unique:
+        username = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(8))
+        cursor.execute((
+            """SELECT * FROM users WHERE username = %s"""
+        ), (username,))
+        if not cursor.fetchone():
+            check_unique = True
+    
+    cursor.execute((
+        """INSERT INTO users (username, guest) VALUES (%s, TRUE)"""
+    ), (username,))
+    return username
+    
 
 def hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
@@ -30,10 +47,10 @@ def verify_password(password: str, hashed: str) -> bool:
     return bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
 
 def main():
-    create_user_with_email("seabuckthorn", "keklolazaza92@gmail.com", "keklolazaza92")
+    create_user_with_email("aa", "NULL", "NULL")
 
 if __name__ == "__main__":
     main()
 connection.commit()
 cursor.close()
-connection.close()
+connection.close()  
--- database/init-scripts/02-sample-data.sql
+++ database/init-scripts/02-sample-data.sql
@@ -1,8 +1,12 @@
--- Copyright (c) 2023 Andrejs Grišins, Anastasia Petrova. Unauthorized use prohibited.
+
+-- Copyright (c) 2025 Andrejs Grišins, Anastasia Petrova. Unauthorized use prohibited.
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id SERIAL PRIMARY KEY,
-	email VARCHAR(100) UNIQUE,
+    guest BOOLEAN DEFAULT FALSE,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
+	email VARCHAR(100) UNIQUE,
+    password_hash VARCHAR(255),
+	CONSTRAINT email_required_if_not_guest CHECK (guest = TRUE OR email IS NOT NULL),
+	CONSTRAINT password_required_if_not_guest CHECK (guest = TRUE OR password_hash IS NOT NULL)
 );
+    
