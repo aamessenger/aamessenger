5a1cd95a3c1b43d5851e23d92d4651e45b621978
andrejsgrisins <andrejsgrisins2004@gmail.com>
2025-09-04 14:47:17 +0300

    Database configuration added (Docker)

--- backend/repositories/UserRepository.kt
+++ backend/repositories/UserRepository.kt
@@ -1,16 +1,21 @@
 // Copyright (c) 2023 Andrejs Grišins, Anastasia Petrova. Unauthorized use prohibited.
+import org.postgresql.ds.PGSimpleDataSource
+import org.jetbrains.exposed.sql.Database
 
 class UserRepository(private val config: AuthConfig) {
     val db = Database.connect(
-        url = config.dbUrl,
-        user = config.dbUser,
-        password = config.dbPassword
-        databaseName = "postgres"
+        PGSimpleDataSource().apply {
+            setURL(config.dbUrl)
+            user = config.dbUser
+            password = config.dbPassword
+            databaseName = config.dbName
+        }
     )
     
     fun insertUser(username: String, email: String, hashedPassword: String, salt: String, isGuest: Boolean) {
 
     }
+
     // Placeholder for actual database interaction code
     fun findByUsernameOrEmail(usernameOrEmail: String): User? {
         // Implement database lookup logic here
