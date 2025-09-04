4a10144b11989c48b9105f64e925f509637ba5d5
andrejsgrisins <andrejsgrisins2004@gmail.com>
2025-09-05 01:23:36 +0300

    Backend structure changed.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,9 @@
 dependencies {
-    implementation("org.jetbrains.exposed:exposed-core:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.41.1")
-    implementation("org.postgresql:postgresql:42.5.4")
+    implementation "org.jetbrains.exposed:exposed-core:$exposedVersion"
+    implementation "org.jetbrains.exposed:exposed-jdbc:$exposedVersion"
+    implementation "org.jetbrains.exposed:exposed-java-time:$exposedVersion"
+    implementation "org.postgresql:postgresql:42.5.4"
+    implementation "com.zaxxer:HikariCP:5.0.1"
+
+    //implementation "org.jetbrains.exposed:exposed-dao:$exposedVersion" 
 }
